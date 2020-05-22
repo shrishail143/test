4bb0f144a4d2abaf7b4558d4aa67c373fefe03a4
shrishail143 <56339610+shrishail143@users.noreply.github.com>
2020-05-22 16:13:18 +0530

    first commit

---
+++ ArrayList1/src/ArrayList/Array.java
@@ -0,0 +1,17 @@
+package ArrayList;
+
+public class Array {
+
+	public static void main(String[] args) {
+		
+		String[] student = new String[3];
+		student[0] = "hari";
+		student[1] = "om";
+		student[2] = "shri";
+		
+		System.out.println(student[1]);
+                System.out.println(student[2]);
+                System.out.println(student[3]);
+	}
+
+}
